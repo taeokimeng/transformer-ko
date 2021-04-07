ea3745870341fb8657e910feafb07b475ec685fc
Taeo Kim <kweras95@gmail.com>
2021-04-07 13:44:15 +0900

    Add configurations

---
+++ config.py
@@ -0,0 +1,25 @@
+# Data path and file name
+DATA_PATH = './data/'
+CHAT_BOT_DATA_FILE_NAME = 'ChatBotData.csv'
+VOCAB_FILE_NAME = 'vocab_entire_data'
+
+# Sentence max length
+MAX_LENGTH = 40
+
+# Batch
+BATCH_SIZE = 64
+BUFFER_SIZE = 20000
+
+# Hyper parameters
+D_MODEL = 256
+NUM_LAYERS = 2
+NUM_HEADS = 8
+DFF = 512
+DROPOUT = 0.1
+
+# Epochs
+EPOCHS = 2  # 50
+
+# Model save path and version
+MODEL_SAVE_PATH = './models/transformer/'
+MODEL_VERSION = '0'
